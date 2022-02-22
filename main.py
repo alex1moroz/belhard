d1f9eb499ca8a7903d91774aba838aba00fc3982
alex1moroz <your-email-address>
2022-02-22 23:08:03 +0300

    add user

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-name = 'Alexanrd'
-surname = 'Moroz'
-age = 28
-belhard_student = False
+import user
+
+if user.belhard_student:
+    status = "I am not a Belhard student"
+else:
+    status = "I am a Belhard student"
+
+print(f"My name is {user.name} {user.surname}\n"
+      f"I am {user.age} years old\n"
+      f"{status}")
---
+++ user.py
@@ -0,0 +1,4 @@
+name = 'Alexanrd'
+surname = 'Moroz'
+age = 28
+belhard_student = False
